c0b09f09a94734a59f95c8bbb5d3273911b875d4
max <max314@mail.ru>
2015-06-15 07:26:19 +0300

    comas

--- app/src/main/java/ru/max314/testvtm/MainActivity.java
+++ app/src/main/java/ru/max314/testvtm/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import org.oscim.android.MapActivity;
 import org.oscim.android.MapScaleBar;
@@ -26,6 +28,21 @@ public class MainActivity extends MapActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MapView mMapView = (MapView) this.findViewById(R.id.mapView);
+        ImageView imageView = (ImageView) this.findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    int y= 10;
+            }
+        });
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                int y=19;
+                return false;
+            }
+        });
+
 
         Map map = this.map();
 
@@ -35,9 +52,9 @@ public class MainActivity extends MapActivity {
         VectorTileLayer baseLayer = map.setBaseMap(source);
         map.layers().add(new BuildingLayer(map, baseLayer));
         map.layers().add(new LabelLayer(map, baseLayer));
-        map.layers().add(new MapScaleBar(mMapView));
-        //mMap.setTheme(VtmThemes.NEWTRON);
-        map.setTheme(VtmThemes.DEFAULT);
+        map.layers().add(new LayerScaleBar(mMapView));
+        mMap.setTheme(VtmThemes.NEWTRON);
+        //map.setTheme(VtmThemes.DEFAULT);
 
         //mMap.setMapPosition(49.417, 8.673, 1 << 17);
         map.setMapPosition(47.20031256, 38.91554832, 1 << 16);
